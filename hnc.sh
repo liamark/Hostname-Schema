ec7d276582a3ece73c3f2a145ba4b70f263cf40b
Liam Mentha <lmm@pc-ops-sdpit-b31.host.dev.ctp.local>
2017-08-04 14:01:33 +0000

    addition of test 4

--- hnc.sh
+++ hnc.sh
@@ -9,7 +9,7 @@
 # LMM  | 24/07/2017 | Initial research and implementations
 # LMM  | 01/08/2017 | Added Array for seperate tests & Tested lengh of input
 # LMM  | Wed Aug  2 10:16:35 UTC 2017 | Addition of test 2
-# LMM  | Fri Aug  4 07:32:17 UTC 2017 | test 2 edits and test 3 implementation
+# LMM  | Fri Aug  4 07:32:17 UTC 2017 | test 2 edits, test 3+4 implementation
 ################################################################################
 #
 # Description:
@@ -17,15 +17,11 @@
 # [CHAR(2)]-[VARCHAR(<=6)]-[VARCHAR(<=5)]-[CHAR(1)][DECIMAL(2,0)].DOMAIN.LOCAL
 #
 # Plan:
-# 1st test for 253 ASCII characters or less total
-# 2nd test check for and identify any illegal chars;
-#    whitespace, symbols & punctuation (could just test legal chars)
-# 3rd test for max 3 labels delimited by . with a max of 63 characters each
-#    and minimum of 1 (does 63 include the .   ??) I could use an array for
-#       this test for illegality of hostname vs rfc1123 & the hostname schema 
-#         above (can't have more than 4 labels is 1 fail, more then 3
-#            according schema above is another)
-# 4th test labels for illegal hyphens at start and end of labels
+# done | 1st test lengh of input is 253 or less chars
+# done | 2nd test check for and identify any illegal chars;
+#        whitespace, symbols & punctuation (could just test legal chars)
+# done | 3rd test for max 3 labels delimited by . with a max of 63 characters each
+# done | 4th test labels for illegal hyphens at start and end of labels
 # 5th test for failures of .DOMAIN & .LOCAL labels with correct error for each
 # 6th delimit labels by hyphens in array to test against schema
 # 7th test for individual failures of delimited first label seperated by hyphens
@@ -51,7 +47,7 @@ echo $mystring
 ################################################################################
 
 # Lengh
-if [ ${#mystring} -lt 254 ] ;then
+if [ ${#mystring} -lt 254 ]; then
   echo -e "Hostname lengh253 Test: $pass"
 else
   echo -e "Hostname lengh253 Test: $fail"
@@ -60,7 +56,7 @@ fi
 
 
 # Whitespace
-if [[ $mystring =~ [[:space:]] ]] ;then 
+if [[ $mystring =~ [[:space:]] ]]; then 
    echo -e "Whitespace test: $fail"
 else 
    echo -e "Whitespace test: $pass"
@@ -69,7 +65,7 @@ fi
 
 
 # Punctuation & Symbols
-if [[ $mystring =~ [^[:alnum:]\.-]+ ]] ;then
+if [[ $mystring =~ [^[:alnum:]\.-]+ ]]; then
    echo -e "Symbol test: $fail"
 else
    echo -e "Symbol test: $pass"
@@ -81,22 +77,45 @@ fi
 set -f
    # avoid globing (expansion of *)
 segments=(${mystring//./ })
-echo "There are ${#segments[@]} segments"
+#echo "There are ${#segments[@]} segments"
 
 
 
-# Segment test
-if [ ${#segments[@]} -lt 4 ] ;then
+# Subdomain count
+if [ ${#segments[@]} -lt 4 ]; then
    echo -e "Subdomain count test: $pass"
 else
    echo -e "Subdomain count test: $fail"
 fi
 
+
+
+# Subdomain lenghs
+for i in ${!segments[@]}; do
+   if [[ ${#segments[i]} -lt 64 ]]; then
+      echo -e "Subdomain $((i+1)) lengh test: $pass"
+   else
+      echo -e "Subdomain $((i+1)) lengh test: $fail"
+   fi
+done
+
+
+
+# Subdomain illegal hyphens
+for i in ${!segments[@]}; do
+   if [[ ${segments[i]} =~ ^\-+|\-\-+|\-+$ ]]; then
+      echo -e "Subdomain $((i+1)) illegal hyphen test: $fail"
+   else
+      echo -e "Subdomain $((i+1)) illegal hyphen test: $pass"
+   fi
+done
+
+
+
+# check for domain and .local
+
+exit
 seg1=${segments[0]}
 seg2=${segments[1]}
 seg3=${segments[2]}
-
-echo $seg1
-echo $seg2
-echo $seg3
 
